1be4540ff948ed3dc1ca15532408cc4da26211fd
Aleksandr-12 <kvazar-12@mail.ru>
2021-04-02 21:14:58 +0700

    Add files via upload

--- WebEasy/View/index.php
+++ WebEasy/View/index.php
@@ -1,8 +1,16 @@
 <div class="jumbotron jumbotron-fluid">
   <div class="container">
 		<div> Пользователь: <?php echo $_SESSION['user']?></div>
-		<div>История доверности Экстрасенс 1:  <?php echo $_SESSION["history_extra_1"]?>%</div>
-		<div>История доверности Экстрасенс 2:  <?php echo $_SESSION["history_extra_2"]?>%</div>
+		<div>История доверности Экстрасенс 1:  
+		<?php if($_SESSION["history_extra_1"]):
+			echo $_SESSION["history_extra_1"];
+			else: echo 0;
+			endif;?>%</div>
+		<div>История доверности Экстрасенс 2:  
+		<?php if($_SESSION["history_extra_2"]):
+			echo $_SESSION["history_extra_2"];
+			else: echo 0;
+			endif;?>%</div>
 	    <table class="table table-striped">
 	  <thead>
 		<tr>
@@ -32,7 +40,7 @@
 
  	  <form action="/rand"  method="get">
   		  <div class="form-group">
-			<label for="exampleInputName">Число</label>
+			<label for="exampleInputName">Введите число</label>
 			<input name="number" type="number" class="form-control"  placeholder="Введите число" required>
 		 </div>
         <button type="submit" class="btn btn-primary">Отправить</button>
---
+++ WebEasy/View/res_notif.php
@@ -0,0 +1,7 @@
+﻿<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+   <div>Экстрасенс 1: <?php echo $_SESSION["ex_res_1"]?></div>
+	<div> Экстрасенс 2:  <?php echo $_SESSION["ex_res_2"]?></div>
+	<a href="/" class="btn-succes" >OK</a>
+  </div>
+</div>
