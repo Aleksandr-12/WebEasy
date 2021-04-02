d662e1b511b3db4d364efcaac020ce6fc3f22881
Aleksandr-12 <kvazar-12@mail.ru>
2021-04-02 21:16:36 +0700

    Add files via upload

--- WebEasy/Model/model_class.php
+++ WebEasy/Model/model_class.php
@@ -22,8 +22,11 @@ class model_class{
 	
 	public function getDostovernost(){
 		$count = count($_SESSION['array_result']);
-		$_SESSION["history_extra_1"] =  $_SESSION['count_true_extra_1'] / $count * 100;
-		$_SESSION["history_extra_2"] = $_SESSION['count_true_extra_2'] / $count * 100;
+		if($_SESSION['count_true_extra_1'] OR $_SESSION['count_true_extra_2']){
+			$_SESSION["history_extra_1"] =  $_SESSION['count_true_extra_1'] / $count * 100;
+			$_SESSION["history_extra_2"] = $_SESSION['count_true_extra_2'] / $count * 100;
+		}
+		
 		
 	}
 	
@@ -34,6 +37,12 @@ class model_class{
 			$array_result = $_SESSION['array_result'];
 		}
 		if($number == $dogatka_extra_1){
+			if($_SESSION['ex_res_1']){
+				unset($_SESSION['ex_res_1']);
+			}
+			$_SESSION['ex_res_1'] = 'угадал';
+			$_SESSION['ex_res_2'] = 'не угадал';
+			
 			if(!$_SESSION['count_true_extra_1']){
 				$_SESSION['count_true_extra_1'] = 1;
 			}else{
@@ -41,6 +50,13 @@ class model_class{
 			}
 			array_push($array_result,array('dogatka_extra_1' => 'угадал','dogatka_extra_2' => 'не угадал','number' => $number));
 		}elseif($number == $dogatka_extra_2){
+			
+			if($_SESSION['ex_res_2']){
+				unset($_SESSION['ex_res_2']);
+			}
+			$_SESSION['ex_res_1'] = 'не угадал';
+			$_SESSION['ex_res_2'] = 'угадал';
+			
 			if(!$_SESSION['count_true_extra_2']){
 				$_SESSION['count_true_extra_2'] = 1;
 			}else{
@@ -49,6 +65,17 @@ class model_class{
 			array_push($array_result,array('dogatka_extra_1' => 'не угадал','dogatka_extra_2' => 'угадал','number' => $number));
 		}else{
 			array_push($array_result,array('dogatka_extra_1' => 'не угадал','dogatka_extra_2' => 'не угадал','number' => $number));
+			
+			if($_SESSION['ex_res_1']){
+				unset($_SESSION['ex_res_1']);
+
+			}
+			if($_SESSION['ex_res_2']){
+
+				unset($_SESSION['ex_res_2']);
+			}
+			$_SESSION['ex_res_1'] = 'не угадал';
+			$_SESSION['ex_res_2'] = 'не угадал';
 		}
 		$_SESSION['array_result'] = $array_result;
 	}
