391d4f47983382bb6c7cc58e68350717ccef8004
Aleksandr-12 <kvazar-12@mail.ru>
2021-04-02 21:29:30 +0700

    Add files via upload

--- WebEasy/View/res_notif.php
+++ WebEasy/View/res_notif.php
@@ -2,6 +2,6 @@
   <div class="container">
    <div>Экстрасенс 1: <?php echo $_SESSION["ex_res_1"]?></div>
 	<div> Экстрасенс 2:  <?php echo $_SESSION["ex_res_2"]?></div>
-	<a href="/" class="btn-succes" >OK</a>
+	<a href="/" role="button" class="btn btn-primary" >OK</a>
   </div>
 </div>
