e671bf2e82610fbe5446ee2ddf6f426667d9c934
Aleksandr-12 <kvazar-12@mail.ru>
2021-04-09 14:48:11 +0700

    Delete res_notif.php

--- WebEasy/View/res_notif.php
+++
@@ -1,7 +0,0 @@
-﻿<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-   <div>Экстрасенс 1: <?php echo $_SESSION["ex_res_1"]?></div>
-	<div> Экстрасенс 2:  <?php echo $_SESSION["ex_res_2"]?></div>
-	<a href="/" role="button" class="btn btn-primary" >OK</a>
-  </div>
-</div>
